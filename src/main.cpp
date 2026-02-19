87393b6d25c3129deae026c988c109f4d9c98594
Sneha539 <sneha.singh_cs.aiml24@gla.ac.in>
2026-02-19 15:30:00 +0530

    added setup function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@ Servo MYSERVO;
 int trigPin = 9;
 int echoPin = 10;
 void setup() {
+    pinMode(trigPin,OUTPUT);
+    pinMode(echoPin,INPUT);
+    MYSERVO.attach(5);
+    Serial.begin(9600);
 // write your initialization code here
 
 }
